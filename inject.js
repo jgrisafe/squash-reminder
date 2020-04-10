4dde573fe1dfeb31da0322d9f96d06ca06516a03
Joey Grisafe <jgrisafe@lunadna.com>
2020-04-09 17:10:44 -0700

    fixed for recent github changes

--- inject.js
+++ inject.js
@@ -1,23 +1,22 @@
 (function() {
 	var counter = document.getElementById('commits_tab_counter');
-	var buttonClass = 'js-merge-branch-action';
-	var stillOpen = document.getElementById('partial-pull-merging').innerHTML.indexOf(buttonClass) !== -1;
-	var mergeDetails = document.getElementsByClassName('mergeability-details');
-	var mergeArea = mergeDetails[0];
-	if (!mergeArea) {
-	  console.log("WTF");
-	}
+	var mergeMessage = document.getElementsByClassName('merge-message');
+	var mergeMessage = mergeMessage[0];
+	
 	var multipleCommits = counter.innerHTML.trim() !== '1';
 
-	if (/*stillOpen &&*/ multipleCommits) {
-	  var out =
-	    "<div class='branch-action-item'>" +
-		"<div class='branch-action-item-icon completeness-indicator completeness-indicator-error'>" +
-		"<span class='octicon octicon-x'></span>" +
-	      "</div>" +
-	      "<h4 class='status-heading'>Don't forget to squash your commits</h4>" +
-		"<span class='status-meta'><code>git rebase -i</code> on the command line</span>" +
-	    "</div>";
-	  mergeArea.innerHTML = out + mergeArea.innerHTML;
+	if (multipleCommits) {
+		var content = `
+			<div class='branch-action-item'>
+				<div class="branch-action-item-icon completeness-indicator completeness-indicator-error">
+					<svg class="octicon octicon-alert" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 000 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 00.01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"></path></svg>
+				</div>
+				<h4 class='status-heading'>Don't forget to squash your commits</h4>
+			</div>`
+
+		var div = document.createElement('div');
+		div.innerHTML = content.trim();
+		var element = div.firstChild;
+		mergeMessage.parentNode.insertBefore(element, mergeMessage);
 	}
 })()
